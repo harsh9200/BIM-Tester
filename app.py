882f46f602678bb0e09d9af391ea842ba12620c2
Harsh Agarwal <hagarwal9200@gmail.com>
2021-04-08 20:38:35 +0530

    Fixed file_path

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def base():
     global file_path
-    print(file_path)
     if request.method == 'GET':
         with open(file_path, 'r') as f:
             data = json.load(f)
@@ -16,7 +15,6 @@ def base():
         if request.args.get('value'):
             file_path = featuresDict[request.args['value']]
             return redirect(request.path)
-        print(os.path.basename(os.path.dirname(file_path)))
         return render_template("index.html", F=data, feat=os.path.basename(os.path.dirname(file_path)))
     else:
         feature_path = file_path.rsplit('.')[0] + '.feature'
@@ -27,8 +25,6 @@ def base():
                          as_attachment=True)
 
 if __name__ == '__main__':
-    global file_path
     featuresDict = Generate_JSON().features_dict()
     file_path = featuresDict['project_setup']
-    print(file_path)
     app.run()
