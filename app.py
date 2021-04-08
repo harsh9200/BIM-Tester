9eba745c74fa93d58c37432b334f1797c4559c2f
Harsh Agarwal <hagarwal9200@gmail.com>
2021-04-08 20:47:27 +0530

    Try 2: FIxing file_path

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ from flask import Flask, render_template, request, send_file, redirect
 
 app = Flask(__name__)
 
+featuresDict = Generate_JSON().features_dict()
+
 @app.route('/', methods=['GET', 'POST'])
 def base():
-    global file_path
+    file_path = featuresDict[request.args.get('value', 'project_setup')]
     if request.method == 'GET':
         with open(file_path, 'r') as f:
             data = json.load(f)
         
-        if request.args.get('value'):
-            file_path = featuresDict[request.args['value']]
-            return redirect(request.path)
+        
         return render_template("index.html", F=data, feat=os.path.basename(os.path.dirname(file_path)))
     else:
         feature_path = file_path.rsplit('.')[0] + '.feature'
@@ -25,6 +25,4 @@ def base():
                          as_attachment=True)
 
 if __name__ == '__main__':
-    featuresDict = Generate_JSON().features_dict()
-    file_path = featuresDict['project_setup']
     app.run()
